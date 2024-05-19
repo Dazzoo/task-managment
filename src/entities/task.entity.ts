88676f65cda5e453fddf564122046d6426082e10
Yurii Shushanskyi <yuraks46@gmail.com>
2024-05-19 20:17:14 +0300

    interceptors and serialize user data for task entity

--- src/entities/task.entity.ts
+++ src/entities/task.entity.ts
@@ -1,6 +1,8 @@
 import { TaskStatus } from "src/tasks/types/tasks";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./users.entity";
+import { Exclude } from 'class-transformer';
+
 
 @Entity()
 export class Task {
@@ -18,5 +20,6 @@ export class Task {
     status: TaskStatus
 
     @ManyToOne(_type => User, user => user.tasks, { eager: true })
+    @Exclude({ toPlainOnly: true })
     user: User
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './logging.interceptor';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor(), new TransformInterceptor())
+  
   await app.listen(3000);
 }
 bootstrap();
