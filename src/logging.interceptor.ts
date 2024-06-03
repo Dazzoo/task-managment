686a8fa7aad1a646df400db0e97a29f80b8c8a99
Yurii Shushanskyi <yuraks46@gmail.com>
2024-06-03 23:41:07 +0300

    add logging

--- src/logging.interceptor.ts
+++ src/logging.interceptor.ts
@@ -1,17 +1,24 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
 
+    const logger = new Logger()
+
+    const httpContext = context.switchToHttp();
+    const request = httpContext.getRequest();
+
+    // Access user from the request object
+    const user = request.user;
+    
     const now = Date.now();
     return next
       .handle()
       .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)),
+        tap(() => logger.verbose(`Request from [${user.username}] user in [${context.getHandler().name}] handler ${Date.now() - now}ms`)),
       );
   }
 } 
