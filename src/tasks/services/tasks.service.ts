766ef1a4ba886d91b4b6cfe9c2023b1b449a9dda
Yurii Shushanskyi <yuraks46@gmail.com>
2024-06-03 20:07:42 +0300

    fix getTasksWithFilter query

--- src/tasks/services/tasks.service.ts
+++ src/tasks/services/tasks.service.ts
@@ -44,7 +44,7 @@ export class TasksService {
         }
 
         if (search) {
-            query.andWhere('LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)', {
+            query.andWhere('(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))', {
                 search: `%${search}%`
             })
         }
