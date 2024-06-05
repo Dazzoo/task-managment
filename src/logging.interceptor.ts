53cedb975cab9d2d98a1b4782532e8a1ba04c694
Yurii Shushanskyi <yuraks46@gmail.com>
2024-06-05 17:04:30 +0300

    fix no user in request case

--- src/logging.interceptor.ts
+++ src/logging.interceptor.ts
@@ -1,24 +1,33 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Logger,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-
-    const logger = new Logger()
+    const logger = new Logger();
 
     const httpContext = context.switchToHttp();
     const request = httpContext.getRequest();
 
     // Access user from the request object
     const user = request.user;
-    
+
     const now = Date.now();
     return next
       .handle()
       .pipe(
-        tap(() => logger.verbose(`Request from [${user.username}] user in [${context.getHandler().name}] handler ${Date.now() - now}ms`)),
+        tap(() =>
+          logger.verbose(
+            `Request from [${user.username || 'Not Authorized'}] user in [${context.getHandler().name}] handler ${Date.now() - now}ms`,
+          ),
+        ),
       );
   }
-} 
+}
