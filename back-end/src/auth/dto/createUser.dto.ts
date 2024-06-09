a2149974d70030803b4db5a533639ce68d1cd168
Yurii Shushanskyi <yuraks46@gmail.com>
2024-06-09 21:36:02 +0300

    change username max length

--- back-end/src/auth/dto/createUser.dto.ts
+++ back-end/src/auth/dto/createUser.dto.ts
@@ -1,20 +1,24 @@
-import { IsEmail, IsNotEmpty, Matches, MaxLength, MinLength } from "class-validator"
-
+import {
+  IsEmail,
+  IsNotEmpty,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(40)
+  username: string;
 
-    @IsNotEmpty()
-    @MinLength(3)
-    @MaxLength(16)
-    username: string
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 
-    @IsNotEmpty()
-    @IsEmail()
-    email: string
-
-    @IsNotEmpty()
-    @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, { 
-        message: "Minimum eight characters, at least one letter and one number"
-    })
-    password: string
+  @IsNotEmpty()
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+    message: 'Minimum eight characters, at least one letter and one number',
+  })
+  password: string;
 }
